15c0da54c4642fbc5778b0f13a0fbcb961eb263a
Callum Scott <scott.callum@gmail.com>
2023-11-16 14:49:04 +0000

    fix terraform fmt

--- main.tf
+++ main.tf
@@ -1,42 +1,41 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.23.1"
     }
     http = {
-      source = "hashicorp/http"
+      source  = "hashicorp/http"
       version = "3.4.0"
     }
     local = {
-      source = "hashicorp/local"
+      source  = "hashicorp/local"
       version = "2.4.0"
     }
   }
 }
 
-data "http" "init_status" {
-  url = "http://${var.vault_host}/v1/sys/init"
-  method = "GET"
+locals {
+  vault_host = var.use_tls ? "https://${var.vault_host}" : "http://${var.vault_host}"
 }
 
 resource "null_resource" "init" {
   provisioner "local-exec" {
-    command = "${path.module}/vault_init/bin/python3.12 vault_init/vault_init.py > vault_init.out"
+    command = "${var.python_path} vault_init/vault_init.py > vault_init.out"
     //interpreter = ["${path.module}/vault_init/bin/python3.12"]
     environment = {
-      VAULT_ADDR = "http://127.0.0.1"
-      VAULT_SHARES = 1
-      VAULT_THRESHOLD = 1
-      VAULT_BACKEND = "local"
-      VAULT_STORE = "local"
-      DEBUG = "False"
+      VAULT_ADDR      = local.vault_host
+      VAULT_SHARES    = var.vault_secret_shares
+      VAULT_THRESHOLD = var.vault_secret_threshold
+      VAULT_BACKEND   = var.vault_init_backend
+      VAULT_STORE     = var.vault_init_backend
+      DEBUG           = var.vault_init_debug
     }
   }
 }
 
 data "local_file" "vault_init_log" {
-  filename = "${path.module}/vault_init.out"
+  filename   = "${path.module}/vault_init.out"
   depends_on = [null_resource.init]
 }
 
--- variables.tf
+++ variables.tf
@@ -1,19 +1,57 @@
 variable "vault_host" {
-  type = string
+  type    = string
   default = "127.0.0.1"
 }
 
+variable "python_path" {
+  type        = string
+  default     = "/usr/bin/python3"
+  description = "Path to python executable"
+}
+
+variable "use_tls" {
+  type        = bool
+  default     = true
+  description = "Use TLS connection when connecting to Vault"
+}
+
+variable "vault_init_backend" {
+  type        = string
+  default     = "local"
+  description = "Vault init backend type"
+  validation {
+    condition     = contains(["local", "aws"], var.vault_init_backend)
+    error_message = "Vault init backend must be one of: local, aws"
+  }
+}
+
+variable "vault_init_store" {
+  type        = string
+  default     = "local"
+  description = "Vault init store type"
+  validation {
+    condition     = contains(["local", "aws_s3", "parameter_store"], var.vault_init_store)
+    error_message = "Vault init store must be one of: local, aws_s3, parameter_store"
+  }
+}
+
+variable "vault_init_debug" {
+  type        = string
+  default     = "False"
+  description = "Enable debug mode for init script"
+}
+
 variable "vault_secret_shares" {
-  type = number
+  type    = number
   default = 5
 }
 
 variable "vault_secret_threshold" {
-  type = number
+  type    = number
   default = 3
 }
 
 variable "vault_unseal_key" {
-  type = string
+  type    = string
   default = "my-unseal-key"
 }
